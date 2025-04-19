e84e51fb913dc0be3ed810673f915045c110565c
Chirag Sawant <101026556+venev-g@users.noreply.github.com>
2025-04-19 02:46:24 +0000

    Add chatbot implementation and API key configuration

---
+++ chatbot.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+import google.generativeai as genai
+from langchain_community.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+
+# Get the API key from Streamlit secrets
+api_key = st.secrets["GOOGLE_API_KEY"]
+
+# Configure the API key
+genai.configure(api_key=api_key)
+
+
