2828b38818cd6951adcc47e24134789e8d11224f
Chirag Sawant <101026556+venev-g@users.noreply.github.com>
2025-04-19 03:08:05 +0000

    Refactor chatbot functionality: enhance PDF text extraction, implement vector store creation, and improve conversational AI chain handling

--- chatbot.py
+++ chatbot.py
@@ -25,7 +25,7 @@ st.caption("Powered by Gemini✨")
 if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "assistant", "content": "Hello! I am a medical chatbot. How can I help you today?"}]
 
-# Function to extract text from PDFs
+# extract text from PDFs
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
@@ -35,8 +35,45 @@ def get_pdf_text(pdf_docs):
             text += page.extract_text()
     return text
 
-# Function to split text into chunks
+# plit text into chunks
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
     chunks = text_splitter.split_text(text)
     return chunks
+
+# create and save FAISS vector store
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    save_path = os.path.expanduser("~/faiss_index")  #writable path
+    vector_store.save_local(save_path)
+
+# load conversational AI chain
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details. If the answer is not in
+    the provided context, just say, "answer is not available in the context"; don't provide a wrong answer.\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash-8b", temperature=0.3, google_api_key=api_key)
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    return chain
+
+# handling user input and generate responses
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004", google_api_key=api_key)
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_question)
+    chain = get_conversational_chain()
+    response = chain.invoke({"input_documents": docs, "question": user_question}, return_only_outputs=True)
+    return response["output_text"]
+
+# display all messages in the chat history
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+
