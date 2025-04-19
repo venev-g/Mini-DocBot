a0c7e2cf98f6c1f0fea037b153096e37aa139ae6
Chirag Sawant <101026556+venev-g@users.noreply.github.com>
2025-04-19 03:01:10 +0000

    Add initial chatbot UI and session state management

--- chatbot.py
+++ chatbot.py
@@ -14,4 +14,14 @@ api_key = st.secrets["GOOGLE_API_KEY"]
 # Configure the API key
 genai.configure(api_key=api_key)
 
+st.set_page_config(page_title="DocBot", page_icon=":robot_face:")
+st.title("🤖Mini DocBot📝")
+st.write(
+    "This is a RAG based mini DocBot that can answer questions based on the content of a PDF document. "
+    "You can upload a PDF file, and the bot will extract the text and provide answers to your questions."
+)
+st.caption("Powered by Gemini✨")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "assistant", "content": "Hello! I am a medical chatbot. How can I help you today?"}]
 
