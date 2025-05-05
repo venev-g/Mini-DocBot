ad062971ee3d0d7234a505c506a71ac1bf298d59
Chirag Sawant <101026556+venev-g@users.noreply.github.com>
2025-05-05 23:18:23 +0530

    Adjusted chunk size, chunk overlap

--- chatbot.py
+++ chatbot.py
@@ -40,7 +40,7 @@ def get_pdf_text(pdf_docs):
 
 # Split text into chunks
 def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
     chunks = text_splitter.split_text(text)
     return chunks
 
